0833ea32814f280e66d2758663141461378b10ae
cg-dv <cgdevproject@gmail.com>
2021-10-11 13:30:53 -0700

    Updated ECS file - removed MySQL container, references to EFS, removed efs Terraform file, Secrets Manager file, and variables Terraform file referencing MySQL credentials

--- variables.tf
+++
@@ -1,9 +0,0 @@
-data "aws_secretsmanager_secret_version" "credentials" {
-  secret_id = "arn:aws:secretsmanager:us-east-1:414402433373:secret:tf-3-tier-credentials-kG8z3u"
-}
-
-locals {
-  db_credentials = jsondecode(
-    data.aws_secretsmanager_secret_version.credentials.secret_string
-  )
-}
